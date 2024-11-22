a0cf1c448492b23d012692c33b6e5c9ae48366cf
Jorge Zerpa <jorgezerpacoder@gmail.com>
2024-11-22 09:29:44 -0400

    [feat] client_test -> create utils folder

--- client_test/src/components/Navbar.tsx
+++ client_test/src/components/Navbar.tsx
@@ -7,10 +7,6 @@ import { useDisconnect, useAccount, useStarkProfile, useBalance } from "@starkne
 import { useBlockies } from "@/hooks/useBlockies";  
 import { WalletConnectorModal } from "./WalletConnectModal";
 
-export function shortAddress(address: string): string {
-  return `${address.slice(0, 6)}...${address.slice(-4)}`;
-}
-
 export const Navbar = () => {
 
   const [showUserOptions, setShowUserOptions] = useState(false)
